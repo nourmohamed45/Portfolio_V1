93217309a28a5d670c1a4710a591df4aeeee3458
Nour <nourelagwany45@gmail.com>
2023-11-14 13:19:48 +0200

    Adding Projects

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,32 +1,94 @@
+import { v4 as uuidv4} from "uuid"
+
 export const myProjects = [
   {
-    id: "1",
-    category: ["react-mui", "node-express"],
-    image: "./",
-    projectTitle: "React & Node.js & Express Project",
+    id: uuidv4(),
+    category: ["react-mui"],
+    image: "../../../public/Project-images/React-Anime-Website-V1.png",
+    projectTitle: "React Anime Website",
     description:
-      "lorem ipsum dolor sit amet consectetur elit adipisicing. Ex tempore dolor in, accusantium laudantium accusamus.",
-    preview: "https://www.google.com",
-    github: "https://github.com",
+      "This is anime website for searching about your favourite anime, watching trailer, knowing some details about it and checkout characters.",
+    preview: "https://654e1d6d96c0d774f6ff480b--timely-squirrel-d6cd04.netlify.app/",
+    github: "https://github.com/nourmohamed45/Simple-Anime-Website",
   },
   {
-    id: "2",
+    id: uuidv4(),
     category: ["html-css"],
-    image: "./",
-    projectTitle: "Html & CSS Project",
+    image: "../../../public/Project-images/Greate-Game.png",
+    projectTitle: "Great Game",
     description:
-      "lorem ipsum dolor sit amet consectetur elit adipisicing. Ex tempore dolor in, accusantium laudantium accusamus.",
-    preview: "https://www.google.com",
-    github: "https://github.com",
+      "For web app gamers, Great Game is a one-stop shop. Play new and vintage games, watch trailers, and read news and stories. hundreds of thousands of users can watch live gaming sessions. To suit your demands and budget, pick from a variety of plans. To remain current on the newest gaming trends, join our group.",
+    preview: "https://nourmohamed45.github.io/Great_Game/",
+    github: "https://github.com/nourmohamed45/Great_Game",
   },
   {
-    id: "3",
-    category: ["js"],
-    image: "./",
-    projectTitle: "JavaScript Project",
+    id: uuidv4(),
+    category: ["react-mui"],
+    image: "../../../public/Project-images/React-Todo-App.png",
+    projectTitle: "React Todo",
     description:
-      "lorem ipsum dolor sit amet consectetur elit adipisicing. Ex tempore dolor in, accusantium laudantium accusamus.",
-    preview: "https://www.google.com",
-    github: "https://github.com",
+      "Arabic Todo App ==> using React & Material Ui",
+    preview: "https://653654b4cfb37a007d92eeb3--zingy-cuchufli-592f64.netlify.app/",
+    github: "https://github.com/nourmohamed45/React_Todo_App",
+  },
+  {
+    id: uuidv4(),
+    category: ["js"],
+    image: "../../../public/Project-images/Generation-Password.png",
+    projectTitle: "Password Generator",
+    description:
+      "This GitHub repository hosts a customizable Password Generator Project, designed to enhance security by generating strong and unique passwords tailored to your specific requirements.",
+    preview: "https://65534a08e4bfd94779c02216--melodic-gelato-259d61.netlify.app/",
+    github: "https://github.com/nourmohamed45/Generation-Password",
+  },
+  {
+    id: uuidv4(),
+    category: ["html-css"],
+    image: "../../../public/Project-images/Harry-Potter_(First-website).png",
+    projectTitle: "Harry Potter (First-website)",
+    description:
+      "Introducing the ultimate Harry Potter fan app - your one-stop-shop for everything related to the magical world of Harry Potter! With our app, you can easily browse and explore all of the Harry Potter brands, including movies, games, and books.",
+    preview: "https://6553494dce94d447ba132265--dapper-creponne-fcdb66.netlify.app/",
+    github: "https://github.com/nourmohamed45/Harry_Potter",
+  },
+  {
+    id: uuidv4(),
+    category: ["js"],
+    image: "../../../public/Project-images/Social-media-V1.png",
+    projectTitle: "Social Media App V1",
+    description:
+      "This mini social media web application, tightly integrated with a powerful backend API, offers users a dynamic platform to connect and share. Users can seamlessly perform actions such as user registration, login, and logout. Additionally, they can create, edit, and delete posts, view detailed post information, and engage with the community by adding comments or exploring comments from other users. Users can also browse and view profile details of other members. I did not prioritize design; my primary focus was on the logical aspects of the web application, which I managed using pure JavaScript.",
+    preview: "https://earnest-seahorse-a244c9.netlify.app/",
+    github: "https://github.com/nourmohamed45/Mini_Social_Media",
+  },
+  {
+    id: uuidv4(),
+    category: ["react-mui"],
+    image: "../../../public/Project-images/React-smiple-weather-App.png",
+    projectTitle: "Simple Weather App",
+    description:
+      "simple weather app with Arabic & English",
+    preview: "https://65470f0730cd2a7f7e448ad6--delicate-crumble-38fff1.netlify.app/",
+    github: "https://github.com/nourmohamed45/Simple-weather-react-app",
+  },
+  {
+    id: uuidv4(),
+    category: ["js"],
+    image: "../../../public/Project-images/Tripify-Adventure.png",
+    projectTitle: "Tripify Adventure",
+    description:
+      "Explore the world of travel and adventure with Tripify. Our all-in-one travel companion offers personalized recommendations, real-time travel updates, interactive maps, and a user-friendly interface. Dive into travel stories, discover hidden gems, and embark on unforgettable journeys.",
+    preview: "https://nourmohamed45.github.io/Tripify-adventure/",
+    github: "https://github.com/nourmohamed45/Tripify-adventure",
+  },
+  {
+    id: uuidv4(),
+    category: ["js"],
+    image: "../../../public/Project-images/LLegar-Graduation-Project.png",
+    projectTitle: "LLEGAR (Graduation Project)",
+    description:
+      "At LLEGAR, our top priority is providing reliable, secure, and easy-to-use rental services, while also promoting diversity and inclusivity on our platform. We pride ourselves on offering a wide variety of rental items to meet the diverse needs and preferences of our customers.",
+    preview: "https://classy-rugelach-91f76c.netlify.app/",
+    github: "https://github.com/nourmohamed45/LLEGAR-FRONT-END-main",
   },
 ];
