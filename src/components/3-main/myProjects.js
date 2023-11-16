62d10d0a7364c086d3c3b845dbbaaf0c2ee69456
Nour <nourelagwany45@gmail.com>
2023-11-16 10:25:11 +0200

    add youtube clone project

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid";
 
 export const myProjects = [
   {
@@ -8,7 +8,8 @@ export const myProjects = [
     projectTitle: "React Anime Website",
     description:
       "This is anime website for searching about your favourite anime, watching trailer, knowing some details about it and checkout characters.",
-    preview: "https://654e1d6d96c0d774f6ff480b--timely-squirrel-d6cd04.netlify.app/",
+    preview:
+      "https://654e1d6d96c0d774f6ff480b--timely-squirrel-d6cd04.netlify.app/",
     github: "https://github.com/nourmohamed45/Simple-Anime-Website",
   },
   {
@@ -26,9 +27,9 @@ export const myProjects = [
     category: ["react-mui"],
     image: "./Project-images/React-Todo-App.png",
     projectTitle: "React Todo",
-    description:
-      "Arabic Todo App ==> using React & Material Ui",
-    preview: "https://653654b4cfb37a007d92eeb3--zingy-cuchufli-592f64.netlify.app/",
+    description: "Arabic Todo App ==> using React & Material Ui",
+    preview:
+      "https://653654b4cfb37a007d92eeb3--zingy-cuchufli-592f64.netlify.app/",
     github: "https://github.com/nourmohamed45/React_Todo_App",
   },
   {
@@ -38,7 +39,8 @@ export const myProjects = [
     projectTitle: "Password Generator",
     description:
       "This GitHub repository hosts a customizable Password Generator Project, designed to enhance security by generating strong and unique passwords tailored to your specific requirements.",
-    preview: "https://65534a08e4bfd94779c02216--melodic-gelato-259d61.netlify.app/",
+    preview:
+      "https://65534a08e4bfd94779c02216--melodic-gelato-259d61.netlify.app/",
     github: "https://github.com/nourmohamed45/Generation-Password",
   },
   {
@@ -48,7 +50,8 @@ export const myProjects = [
     projectTitle: "Harry Potter (First-website)",
     description:
       "Introducing the ultimate Harry Potter fan app - your one-stop-shop for everything related to the magical world of Harry Potter! With our app, you can easily browse and explore all of the Harry Potter brands, including movies, games, and books.",
-    preview: "https://6553494dce94d447ba132265--dapper-creponne-fcdb66.netlify.app/",
+    preview:
+      "https://6553494dce94d447ba132265--dapper-creponne-fcdb66.netlify.app/",
     github: "https://github.com/nourmohamed45/Harry_Potter",
   },
   {
@@ -66,9 +69,9 @@ export const myProjects = [
     category: ["react-mui"],
     image: "./Project-images/React-smiple-weather-App.png",
     projectTitle: "Simple Weather App",
-    description:
-      "simple weather app with Arabic & English",
-    preview: "https://65470f0730cd2a7f7e448ad6--delicate-crumble-38fff1.netlify.app/",
+    description: "simple weather app with Arabic & English",
+    preview:
+      "https://65470f0730cd2a7f7e448ad6--delicate-crumble-38fff1.netlify.app/",
     github: "https://github.com/nourmohamed45/Simple-weather-react-app",
   },
   {
@@ -90,5 +93,16 @@ export const myProjects = [
       "At LLEGAR, our top priority is providing reliable, secure, and easy-to-use rental services, while also promoting diversity and inclusivity on our platform. We pride ourselves on offering a wide variety of rental items to meet the diverse needs and preferences of our customers.",
     preview: "https://classy-rugelach-91f76c.netlify.app/",
     github: "https://github.com/nourmohamed45/LLEGAR-FRONT-END-main",
+  },
+  {
+    id: uuidv4(),
+    category: ["react-mui"],
+    image: "./Project-images/YouTube-Clone.png",
+    projectTitle: "YouTube Clone",
+    description:
+      "You can discover youtube videos, channels and watching them and search about what do you want",
+    preview:
+      "https://655414bdeb168e35c757b58d--superlative-gumption-4d7df0.netlify.app/",
+    github: "https://github.com/nourmohamed45/youtube_clone",
   },
 ];
